029ecdd22d8d38b6326ea80a94ef864f471d32a0
Arthur <arthur@arthurvardevanyan.com>
2021-03-24 10:56:42 -0400

    Winston Logger Setup

--- server.js
+++ server.js
@@ -1,11 +1,39 @@
 const Express = require('express');
+const Winston = require('winston');
 const { StatusCodes } = require('http-status-codes');
 
 const app = Express();
 app.use(Express.json());
 
-app.get('/', (req, res) => {
+const winstonLogger = Winston.createLogger({
+  transports: [
+    new Winston.transports.Console({
+      format: Winston.format.combine(
+        Winston.format.timestamp({
+          format: 'YYYY-MM-DD HH:mm:ss',
+        }),
+        Winston.format.json(),
+      ),
+    }),
+  ],
+});
+
+app.get('/', (req, res, next) => {
   res.status(StatusCodes.OK).send('Hello World');
+  next();
+});
+
+app.use('/', (req) => {
+  winstonLogger.log({
+    level: 'info',
+    epochTime: Math.floor(Date.now() / 1000),
+    requestType: req.method,
+    url: req.url,
+    body: req.body,
+    query: req.query,
+    headers: req.headers,
+    dateValidation: 'Function Not Created Yet',
+  });
 });
 
 app.listen(8080);
