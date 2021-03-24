e4f54653928b0108820e7f00dcf7d70c4b7d4d19
Arthur <arthur@arthurvardevanyan.com>
2021-03-24 10:15:26 -0400

    Removal of Deprecated Body Parser

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-const express = require('express');
+const Express = require('express');
 
-const app = express();
+const app = Express();
+app.use(Express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
 app.listen(8080);
+
+module.exports = app;
